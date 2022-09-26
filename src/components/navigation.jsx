ec2c0f6813e0b8f3e4cb9d85adb125b13cf2a244
iysaofficial <youngscientist.iysa@gmail.com>
2022-09-26 10:23:03 +0700

    navbar 2

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -1,44 +1,49 @@
-
-
 export const Navigation = (props) => {
   return (
-    <nav id='menu' className='container-fluid navbar navbar-default navbar-fixed-top '>
-      <div className='container'>
-        <div className='navbar-header '>
+    <div className="container-fluid">
+      <nav
+        id="menu"
+        className="navbar-expand-lg navbar navbar-default navbar-fixed-top "
+      >
+        <div className="navbar-header ">
           <button
-            type='button'
-            className='navbar-toggle collapsed'
-            data-toggle='collapse'
-            data-target='#bs-example-navbar-collapse-1'
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#bs-example-navbar-collapse-1"
           >
-            {' '}
-            <span className='sr-only'>Toggle navigation</span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
-            <span className='icon-bar'></span>{' '}
+            {" "}
+            <span className="sr-only">Toggle navigation</span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
+            <span className="icon-bar"></span>{" "}
           </button>
-          <a className='navbar-brand page-scroll' href='#page-top'>
-          <img src="img/gyifLogo.png" alt="logo" style={{width:'150px',height:'65px', marginTop:'-20px'}}/>
-          </a>{' '}
+          <a className="navbar-brand page-scroll" href="#page-top">
+            <img
+              src="img/gyifLogo.png"
+              alt="logo"
+              style={{ width: "150px", height: "65px", marginTop: "-20px" }}
+            />
+          </a>{" "}
         </div>
 
         <div
-          className='collapse navbar-collapse'
-          id='bs-example-navbar-collapse-1'
+          className="collapse navbar-collapse"
+          id="bs-example-navbar-collapse-1"
         >
-          <ul className='nav navbar-nav navbar-right'>
+          <ul className="nav navbar-nav navbar-right">
             <li>
-              <a href='#page-top' className='page-scroll'>
-              Home
+              <a href="#page-top" className="page-scroll">
+                Home
               </a>
             </li>
             <li>
-              <a href='#features' className='page-scroll'>
+              <a href="#features" className="page-scroll">
                 Event
               </a>
             </li>
             <li>
-              <a href='#about' className='page-scroll'>
+              <a href="#about" className="page-scroll">
                 About
               </a>
             </li>
@@ -48,23 +53,26 @@ export const Navigation = (props) => {
               </a>
             </li> */}
             <li>
-              <a href='#portfolio' className='page-scroll'>
+              <a href="#portfolio" className="page-scroll">
                 Gallery
               </a>
             </li>
             <li>
-              <a href='https://drive.google.com/file/d/1kjEQDn9qmt0WXlWyfDRGR238PNnqkDGT/view?usp=sharing' className='page-scroll'>
+              <a
+                href="https://drive.google.com/file/d/1kjEQDn9qmt0WXlWyfDRGR238PNnqkDGT/view?usp=sharing"
+                className="page-scroll"
+              >
                 Guide Book
               </a>
             </li>
             <li>
-              <a href='#contact' className='page-scroll'>
+              <a href="#contact" className="page-scroll">
                 Contact
               </a>
             </li>
           </ul>
         </div>
-      </div>
-    </nav>
-  )
-}
+      </nav>
+    </div>
+  );
+};
